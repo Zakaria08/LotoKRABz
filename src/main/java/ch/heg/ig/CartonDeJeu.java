f2dd001a3e75e18f6e8dd1c265f7f761a9d50f45
blerdon <blerdon1998@gmail.com>
2022-11-20 14:31:01 +0100

    add getter and setter and attributs

--- src/main/java/ch/heg/ig/CartonDeJeu.java
+++ src/main/java/ch/heg/ig/CartonDeJeu.java
@@ -1,4 +1,42 @@
 package ch.heg.ig;
 
 public class CartonDeJeu {
+
+    private int id ;
+    private int[] ligne1;
+    private int[] ligne2;
+    private int[] ligne3;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int[] getLigne1() {
+        return ligne1;
+    }
+
+    public void setLigne1(int[] ligne1) {
+        this.ligne1 = ligne1;
+    }
+
+    public int[] getLigne2() {
+        return ligne2;
+    }
+
+    public void setLigne2(int[] ligne2) {
+        this.ligne2 = ligne2;
+    }
+
+    public int[] getLigne3() {
+        return ligne3;
+    }
+
+    public void setLigne3(int[] ligne3) {
+        this.ligne3 = ligne3;
+    }
 }
