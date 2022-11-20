72da4fc41fc44fd64c955599b8852ec7db3ce8f5
robin.rolle@he-arc.ch <robin1748>
2022-11-20 15:30:44 +0100

    Add Class Tirage

--- src/main/java/ch/heg/ig/Tirage.java
+++ src/main/java/ch/heg/ig/Tirage.java
@@ -1,4 +1,26 @@
 package ch.heg.ig;
 
+import java.net.Inet4Address;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Tirage {
+
+    Set<Integer> numerosSortis = new HashSet<>();
+
+    public Set<Integer> getNumerosSortis() {
+        return numerosSortis;
+    }
+
+    public void setNumerosSortis(Set<Integer> numerosSortis) {
+        this.numerosSortis = numerosSortis;
+    }
+
+    public void ajouter (Integer num){
+        this.numerosSortis.add(num);
+    }
+    public void toutEffacer (){
+        this.numerosSortis.clear();
+    }
+
 }
