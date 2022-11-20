1026564022b231f10a82c42ff5a4ecaadbea00e7
robin.rolle@he-arc.ch <robin1748>
2022-11-20 16:39:00 +0100

    Update Id type CartonDeJeu

--- src/main/java/ch/heg/ig/business/CartonDeJeu.java
+++ src/main/java/ch/heg/ig/business/CartonDeJeu.java
@@ -2,17 +2,17 @@ package ch.heg.ig.business;
 
 public class CartonDeJeu {
 
-    private int id ;
+    private String id ;
     private int[] ligne1;
     private int[] ligne2;
     private int[] ligne3;
 
 
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(String id) {
         this.id = id;
     }
 
