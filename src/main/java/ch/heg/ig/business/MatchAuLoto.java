bb223871a1c922c41bb4107a0a455f0668f81f12
robin.rolle@he-arc.ch <robin1748>
2022-11-20 16:39:45 +0100

    Add method loadCartons to MatchAuLoto

--- src/main/java/ch/heg/ig/business/MatchAuLoto.java
+++ src/main/java/ch/heg/ig/business/MatchAuLoto.java
@@ -1,4 +1,18 @@
 package ch.heg.ig.business;
 
+import ch.heg.ig.reader.ImportJson;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MatchAuLoto {
+    public Map<String,CartonDeJeu> cartons = new HashMap<>();
+
+    public void loadCartons() {
+        this.cartons = ImportJson.importCartonDeJeu();
+    }
+    public CartonDeJeu getCarton(String id){
+       return  this.cartons.get(id);
+    }
 }
