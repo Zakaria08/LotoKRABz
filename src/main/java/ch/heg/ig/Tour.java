e07694cb8be070505a8611ead96b52598b29ca8e
robin.rolle@he-arc.ch <robin1748>
2022-11-20 15:54:42 +0100

    Add Class Tour

--- src/main/java/ch/heg/ig/Tour.java
+++ src/main/java/ch/heg/ig/Tour.java
@@ -1,4 +1,34 @@
 package ch.heg.ig;
 
 public class Tour {
+    enum PhaseLoto {
+        Quine,
+        DoubleQuine,
+        Carton
+    }
+    PhaseLoto phaseActuelle = PhaseLoto.Quine;
+
+    Tirage tirageLoto = new Tirage();
+
+    public void commencer(){
+        //TODO
+    }
+    public void terminer(){
+        //TODO
+    }
+    public void phaseSuivante(){
+        if(this.phaseActuelle == PhaseLoto.Carton)
+        {
+            this.terminer(); // termine le jeu
+        }
+        else {
+            PhaseLoto[] p = PhaseLoto.values();
+            int i = 0;
+            for (; p[i] != this.phaseActuelle; i++)
+                ;
+            i++;
+            i %= p.length;
+            this.phaseActuelle = p[i];
+        }
+    }
 }
