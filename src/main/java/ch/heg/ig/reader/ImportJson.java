2485650ee617987fcf8e509446635358d9a5cf10
zakaria.omarar <zakaria.omarar@he-arc.ch>
2022-11-20 21:05:54 +0100

    Update remove the main method of the ImportJson

--- src/main/java/ch/heg/ig/reader/ImportJson.java
+++ src/main/java/ch/heg/ig/reader/ImportJson.java
@@ -10,23 +10,21 @@ import java.util.HashMap;
 import java.util.List;
 
 
-
-
 public class ImportJson {
-    public static void main(String[] args) {
-            ListCartonDeJeu listCartonDeJeu = null;
-            HashMap<String, CartonDeJeu> output = null;
-            try {
-                ObjectMapper om = new ObjectMapper();
-                listCartonDeJeu = om.readValue(new File("data/CartonsDeLoto.json"), ListCartonDeJeu.class);
-                output = new HashMap<>();
-                for (CartonDeJeu cartonDeJeu : listCartonDeJeu.getCartonsDeJeu()) {
-                    output.put(cartonDeJeu.getId(), cartonDeJeu);
-                }
-
-            } catch (IOException ex) {
-                System.out.println(ex);
+    public static HashMap<String, CartonDeJeu> importCartonDeJeu() {
+        ListCartonDeJeu listCartonDeJeu = null;
+        HashMap<String, CartonDeJeu> output = null;
+        try {
+            ObjectMapper om = new ObjectMapper();
+            listCartonDeJeu = om.readValue(new File("data/CartonsDeLoto.json"), ListCartonDeJeu.class);
+            output = new HashMap<>();
+            for (CartonDeJeu cartonDeJeu : listCartonDeJeu.getCartonsDeJeu()) {
+                output.put(cartonDeJeu.getId(), cartonDeJeu);
             }
 
+        } catch (IOException ex) {
+            System.out.println(ex);
         }
+        return output;
     }
+}
